4390e6437e5f08b60bd8c2789ba2e9a222b8e3f9
Sean Duffy <seaneduffy@gmail.com>
2016-07-08 17:31:55 -0400

    updated port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 });
 
 http.listen(3000, function(){
-	console.log('listening on *:3000');
+	console.log('listening on *:8008');
 });
 
 let io = require('socket.io')(http),
